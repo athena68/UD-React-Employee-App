aec25777089aa3c7072c7d88050d5fbf5bf39a82
Tuan <tnguy258@ford.com>
2024-04-26 16:55:13 +0700

    create middleware reducer and actions

---
+++ app/src/actions/users.js
@@ -0,0 +1,10 @@
+export const RECEIVE_USERS = "RECEIVE_USERS";
+export const ADD_USER_ANSWER = "ADD_USER_ANSWER";
+export const ADD_USER_QUESTION = "ADD_USER_QUESTION";
+
+export function receiveUsers(users) {
+  return {
+    type: RECEIVE_USERS,
+    users,
+  };
+}
---
+++ app/src/reducers/users.js
@@ -0,0 +1,36 @@
+import {
+  RECEIVE_USERS,
+  ADD_USER_QUESTION,
+  ADD_USER_ANSWER,
+} from "../actions/users";
+
+export default function users(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users,
+      };
+    case ADD_USER_QUESTION:
+      return {
+        ...state,
+        [action.author]: {
+          ...state[action.author],
+          questions: state[action.author].questions.concat(action.qid),
+        },
+      };
+    case ADD_USER_ANSWER:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          answers: {
+            ...state[action.authedUser].answers,
+            [action.qid]: action.answer,
+          },
+        },
+      };
+    default:
+      return state;
+  }
+}
